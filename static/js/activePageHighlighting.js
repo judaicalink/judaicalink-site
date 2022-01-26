66f09b89ae6f4fdcab89a940aa0866398bcad791
bsesic <Benjamin-777@gmx.de>
2022-01-26 13:27:05 +0100

    Active page highlighting improved, comments removed.

--- static/js/activePageHighlighting.js
+++ static/js/activePageHighlighting.js
@@ -4,9 +4,4 @@ page = pathName.split("/").filter(function (e) {return e != null;});
 if (page[1] != null) {
     console.log(page[1]);
     $("a[href='/" + page[1] + "']").addClass("active");
-    $("#" + page[1]).addClass("active");
-    //id = $.find("#" + page[1]);
-    //console.log(id);
-    //$.find(page[1]).addClass('active');
-
 }
