7f566fda729a0471029d414fdc95daafccffb6af
bsesic <Benjamin-777@gmx.de>
2025-03-21 14:48:55 +0100

    Sanitize ids

--- generate_beacon.py
+++ generate_beacon.py
@@ -1,7 +1,7 @@
 """
 Generator for the beacon file.
 This scripts generates the beacon file for the GND IDs.
-It uses a simple  sparql query.
+It uses a simple sparql query.
 It should be run monthly.
 
 (c) Benjamin Schnabel, Stuttgart Media University
@@ -18,29 +18,24 @@ now = datetime.datetime.now()
 def header():
     """
     Generate the header of the beacon file with the current date.
-    return: string
     """
     header_text = """#FORMAT: BEACON
 #PREFIX: https://d-nb.info/gnd/
 #TARGET: https://data.judaicalink.org/data/gnd/{ID}
 #CONTACT: Benjamin Schnabel <b.schnabel@hs-mannheim.de>
-#INSTITUTION: Hochschule Mannheim - University of Applied Sciences
+#INSTITUTION: Technische Hochschule Mannheim - University of Applied Sciences Mannheim
 #MESSAGE: JudaicaLink
 #FEED: https://data.judaicalink.org/dumps/beacon/current/beacon-persons.txt
 #TIMESTAMP: """ + now.strftime("%Y-%m-%dT%H:%M:%SZ") + "\n#UPDATE: monthly\n"
-    # print(header_text)
     return header_text
 
 
 def save_file(text):
     """
     Save the beacon file to the current directory.
-    return: boolean
     """
-    filename = 'beacon-persons.txt'
-    filename = os.path.join('/data/dumps/beacon/current/', filename)
+    filename = '/data/dumps/beacon/current/beacon-persons.txt'
     if os.path.exists(filename):
-        # rename the file and add the current date
         os.rename(filename, filename.replace('.txt', '-') + now.strftime("%Y-%m-%d") + '.txt')
     try:
         with open(filename, 'w') as f:
@@ -53,61 +48,59 @@ def save_file(text):
 
 
 def remove_duplicates(ids):
+    """
+    Removes duplicate entries.
+    """
     return list(dict.fromkeys(ids))
 
-def remove_uris(ids):
-    return [id.split('/')[-1] for id in ids]
+
+def sanitize_ids(ids):
+    """
+    - Removes "NA"
+    - Extracts plain IDs from URIs (e.g., http://d-nb.info/gnd/12345678 -> 12345678)
+    """
+    cleaned_ids = []
+    for id in ids:
+        if id == "NA":
+            continue
+        if '/' in id:
+            cleaned_ids.append(id.split('/')[-1])
+        else:
+            cleaned_ids.append(id)
+    return cleaned_ids
 
 
 def get_gnd_ids():
-    # get all gnd ids from the database
+    """
+    Fetch GND IDs via SPARQL.
+    """
     gnd_ids = []
-
-    queryString = "SELECT * WHERE { ?s ?p ?o. } LIMIT 10"
     sparql = SPARQLWrapper("https://data.judaicalink.org/sparql/query")
-
     sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX gndo: <http://d-nb.info/standards/elementset/gnd#>
-    PREFIX jl: <http://data.judaicalink.org/ontology/>
-    PREFIX dcterms: <http://purl.org/dc/terms/>
-    PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-
     SELECT ?id
     WHERE {
        { GRAPH ?g { ?person gndo:gndIdentifier ?id.} }
-       UNION { GRAPH ?g{ ?person gndo:gndIdentifier ?id. } }
+       UNION { GRAPH ?g { ?person gndo:gndIdentifier ?id. } }
     }
-
-
     """)
-
     try:
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
-
         for result in results["results"]["bindings"]:
-            # if result type is literal, get the value
             if result["id"]["type"] == "literal":
                 gnd_ids.append(result["id"]["value"])
-
-        return remove_duplicates(remove_uris(gnd_ids))
+        return remove_duplicates(sanitize_ids(gnd_ids))
     except Exception as e:
         print('Error fetching data: ', e)
-
     return gnd_ids
-
 
 
 ids = get_gnd_ids()
 header_text = header()
-beacon_text = header_text + "\n".join(ids)
+beacon_text = header_text + "\n" + "\n".join(ids)
 result = save_file(beacon_text)
+
 if result is not None:
     print('Beacon file successfully created at "%s"' % result)
 else:
